bf31b59003cc0fa7781c74f6595d1c40723824b4
antho-firuze <antho.firuze@gmail.com>
2016-05-13 17:13:31 +0700

    updates

--- main.go
+++ main.go
@@ -7,11 +7,20 @@ import (
 	
 	"database/sql"
 	_ "github.com/lib/pq"
+	
+	"encoding/json"
 )
 
 type Page struct {
 	Name string
 	DBStatus bool
+}
+
+type SearchResult struct {
+	Name string
+	Description string
+	Email string
+	ID int
 }
 
 func main() {
@@ -20,6 +29,7 @@ func main() {
 	db, _ := sql.Open("postgres", "host=192.168.0.7 port=5432 user=postgres password=admin123 dbname=postgres sslmode=disable")
 	// db, _ := sql.Open("postgres", "postgres://postgres:admin123@localhost/")
 	_ = "breakpoint"
+	
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
 		p := Page{Name: "Firuze"}
 		
@@ -31,7 +41,20 @@ func main() {
 		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-		db.Close()
+		// db.Close()
+	})
+	
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request){
+		results := []SearchResult{
+			SearchResult{"Jojon", "Standard user", "jojon@gmail.com", 1},
+			SearchResult{"Sruntul", "Premium user", "sruntul@gmail.com", 2},
+			SearchResult{"Cilok", "Pertamax user", "cilok@gmail.com", 3},
+		}
+		
+		encoder := json.NewEncoder(w)
+		if err := encoder.Encode(results); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
 	
   fmt.Println(http.ListenAndServe(":8080", nil))
